d37b4b14936125155f07a34c97b636fc4d0918b2
KarolKulka <siwkakarol@gmail.com>
2021-05-19 13:26:13 +0200

    metadata and thumb file

---
+++ metadata.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id' => 'kulka_oxlearn',
+    'title' => [
+        'de' => 'Nauka modulow DE',
+        'en' => 'Nauka modulow EN',
+    ],
+    'description' => [
+        'de' => 'To jest opis DE',
+        'en' => 'To jest opis EN',
+    ],
+    'thumbnail' => 'out/pictures/thumb.png',
+    'version' => '1.0.0',
+    'author' => 'Karol',
+);
