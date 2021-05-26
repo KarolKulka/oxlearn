8e7574b70d0edcb23612fd73a1ea8f35569b5c0d
KarolKulka <siwkakarol@gmail.com>
2021-05-26 15:21:39 +0200

    custom settings in module

--- views/admin/en/kulka_oxlearn_admin_en_lang.php
+++ views/admin/en/kulka_oxlearn_admin_en_lang.php
@@ -7,5 +7,9 @@ $aLang = array(
     'charset' => 'UTF-8',
     'kulka_oxlearn' => 'Oxid module learn',
 
+    'SHOP_MODULE_GROUP_main' => 'Main settings',
+
+    'SHOP_MODULE_kulkaOxlearnTestSettings' => 'Settings for testing database settings save and retrieve'
+
 
 );
