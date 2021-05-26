47b50bd6e23207af3ec9a2fe774fff0ee5e065c4
KarolKulka <siwkakarol@gmail.com>
2021-05-26 15:25:11 +0200

    custom settings in module

---
+++ views/admin/en/module_options.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$sLangName = 'English';
+
+$aLang = array(
+    'charset' => 'UTF-8',
+
+    'SHOP_MODULE_GROUP_main' => 'Main settings',
+
+    'SHOP_MODULE_kulkaOxlearnTestSettings' => 'Settings for testing database settings save and retrieve'
+
+
+);
