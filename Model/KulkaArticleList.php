aebfca80b5601d7d5ae38b0b5805e4e8ff1d0cf3
KarolKulka <siwkakarol@gmail.com>
2021-06-02 13:18:05 +0200

    change getRandomParentId method name to loadRandomParentId

--- Model/KulkaArticleList.php
+++ Model/KulkaArticleList.php
@@ -4,7 +4,7 @@ namespace Kulka\OxLearn\Model;
 
 class KulkaArticleList extends KulkaArticleList_parent
 {
-    public function getRandomParentId(){
+    public function loadRandomParentId(){
         $this->selectString(
             "SELECT oxarticles.OXPARENTID FROM oxarticles WHERE OXPARENTID != '' ORDER BY RAND() LIMIT 1"
         );
