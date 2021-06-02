47d6d376cf1c0a92edb934afef70c239ca47bf39
KarolKulka <siwkakarol@gmail.com>
2021-06-02 16:28:44 +0200

    Changes after changing extends class

--- Model/KulkaArticle.php
+++ Model/KulkaArticle.php
@@ -4,6 +4,11 @@ namespace Kulka\OxLearn\Model;
 
 class KulkaArticle extends KulkaArticle_parent
 {
+    /**
+     * Method for loading random article from DB
+     *
+     * After changing extension Class form ArticleList to Article method needs to changed
+     */
     public function kulka_loadRandomArticle(){
         $sArticleTable = getViewName('oxarticles');
 
